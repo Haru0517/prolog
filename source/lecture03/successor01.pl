cd19c64310b2d360d1eeadb6eeac0c50f364c1e0
Yuto Chikazawa <chikazawa@mm.doshisha.ac.jp>
2019-07-21 23:51:19 +0900

    add nat2int , int2nat

--- source/lecture03/successor01.pl
+++ source/lecture03/successor01.pl
@@ -13,4 +13,16 @@ X = succ(succ(succ(0)))
 "
 
 add(0,X,X).
-add(succ(X), Y, succ(Z)) :- add(X, Y, Z)
+add(succ(X), Y, succ(Z)) :- add(X, Y, Z).
+
+
+nat2int(0,0).
+nat2int(succ(X),Int):- nat2int(X,Int1), Int is Int1+1.
+
+"
+?- nat2int(succ(succ(0)),N).
+N = 2.
+"
+
+int2nat(0,0).
+int2nat(Int,succ(X)):- int2nat(Int1, X), Int is Int1+1, !.
