7c55056f014fd5e610da16ea458deb008eeeb85e
Yuto Chikazawa <chikazawa@mm.doshisha.ac.jp>
2019-07-22 01:02:32 +0900

    fix comment out

--- source/lecture03/successor01.pl
+++ source/lecture03/successor01.pl
@@ -1,7 +1,7 @@
 nat(0).
 nat(succ(X)) :- nat(X).
 
-"
+/*
 ?- nat(succ(succ(succ(0)))).
 true.
 
@@ -10,7 +10,7 @@ X = 0 ;
 X = succ(0) ;
 X = succ(succ(0)) ;
 X = succ(succ(succ(0))) 
-"
+*/
 
 add(0,X,X).
 add(succ(X), Y, succ(Z)) :- add(X, Y, Z).
@@ -19,10 +19,10 @@ add(succ(X), Y, succ(Z)) :- add(X, Y, Z).
 nat2int(0,0).
 nat2int(succ(X),Int):- nat2int(X,Int1), Int is Int1+1.
 
-"
+/*
 ?- nat2int(succ(succ(0)),N).
 N = 2.
-"
+*/
 
 int2nat(0,0).
 int2nat(Int,succ(X)):- int2nat(Int1, X), Int is Int1+1, !.
